a30692293da686f62547d470651a3f7faed6e174
kittswaye <153619900+kittswaye@users.noreply.github.com>
2024-09-16 22:55:02 +0300

    node.js homework 01

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+    const originalContacts = await readContacts();
+    originalContacts.push(createFakeContact());
+    await writeContacts(originalContacts);
+};
 
 addOneContact();
